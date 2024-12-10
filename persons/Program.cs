98ae032d9a146dc704ee35dfba5ed4a9e2679b39
Iris <148431646+iris915520@users.noreply.github.com>
2024-12-10 15:49:24 +0200

    create person class with two methods and call in the main

---
+++ persons/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace persons;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Person person = new();
+        person.AskUserNames();
+        person.Print();
+
+    }
+}
+
+class Person
+{
+    readonly List<string> persons = [];
+
+    public List<string> AskUserNames()
+    {
+        for (int i = 1; i < 4; i++)
+        {
+            Console.Write($"Enter username {i}:");
+            string? username = Console.ReadLine();
+            if (username != null)
+            {
+                persons.Add(username);
+            }
+
+        }
+        return persons;
+    }
+
+    public void Print()
+    {
+        foreach (string person in persons)
+        {
+            Console.WriteLine(person);
+        }
+    }
+
+}
+
+
+
+
+
+
